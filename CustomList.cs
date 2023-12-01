df81a1e3b631047671d7a6dedb113ac62719cb76
Ragimov Ragim <rahimmr@code.edu.az>
2023-12-01 14:46:35 +0400

    CustomListTask

---
+++ CustomList.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+class CustomList<T>
+{
+    private T[] list;
+    private int capacity;
+    private int count;
+    public int Capacity
+    {
+        get { return capacity; }
+    }
+    public int Count
+    {
+        get { return count; }
+    }
+
+    
+
+    public CustomList()
+    {
+        count = 0;
+        capacity = 0;
+        
+    }
+    public void Add(T item)
+    {
+        if (count == capacity)
+        {
+            if (capacity == 0)
+                capacity = 4;
+            else
+                capacity *= 2;
+            T[] newItems = new T[capacity];
+            for (int i = 0; i < count; i++)
+            {
+                newItems[i] = list[i];
+            }
+            list = newItems;
+        }
+        list[count] = item;
+        count++;
+    }
+
+    
+    public T FindItem(Predicate<T> predicate)
+    {
+        return Array.Find(list,  predicate);
+    }
+
+    public List<T> FindAllItems(Predicate<T> predicate)
+    {
+        T[] foundItems =
+            Array.FindAll(list.ToArray(), predicate);
+        return new List<T>(foundItems);
+    }
+    
+    public bool ContainsItem(T item)
+    {
+      if(count !=0 && Array.IndexOf(list, item) != -1)
+        {
+            return true;
+        }
+        return false;
+    }
+    public bool Exists(Predicate<T> match)
+    {
+
+        return Array.Exists(list, match);
+    }
+
+
+
+    public void Remove(T item)
+    {
+        int index = Array.IndexOf(list, item);
+
+        if (index != -1)
+        {
+            for (int i = index; i < count - 1; i++)
+            {
+                list[i] = list[i + 1];
+            }
+            
+
+
+        }
+        count--;
+
+
+    }
+    }
+
+
---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿CustomList<int> customlist = new CustomList<int>();
+
+customlist.Add(5);
+customlist.Add(10);
+customlist.Add(15);
+customlist.Add(20);
+customlist.Add(100);
+
+
+
+customlist.Remove(3);
+customlist.Remove(4);
+Console.WriteLine( customlist.Count);
+
+
+//Console.WriteLine(customlist.Capacity);
+//Console.WriteLine(customlist.Count);
